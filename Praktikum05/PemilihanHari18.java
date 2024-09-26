829f707aa640e8bb3ed6bb863e4e0895f8817662
Calon-raja-ngoding <nbilhnief@gmail.com>
2024-09-26 09:35:13 +0800

    Percobaan 2

---
+++ Praktikum05/PemilihanHari18.java
@@ -0,0 +1,29 @@
+package Praktikum05;
+import java.util.Scanner;
+public class PemilihanHari18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dayName, dayType ;
+
+        System.out.print(" Input day name : ");
+        dayName = sc.nextLine();
+
+        switch (dayName.toLowerCase()) {
+            case "monday":
+            case "tuesday" :
+            case "wednesday":
+            case "thursday":
+            case "friday":    
+            dayType = " weekday " ;
+                break;
+            case "saturday":
+            case "sunday" :
+            dayType = " weekend";    
+            default:
+            dayType = "invalid day name";
+                break;
+        }
+
+        System.out.print(dayName + " is a " + dayType);
+    }
+}
